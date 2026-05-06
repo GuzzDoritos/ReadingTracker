ec84f9e44bdd8eb4fcd1492b14a790582ff78a91
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-06 11:17:55 -0300

    Move Book to Data and add BookID property

---
+++ ReadingTracker/Data/Book.cs
@@ -0,0 +1,35 @@
+﻿namespace ReadingTracker.Data
+{
+    internal class Book
+    {
+        public int BookID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int TotalChars { get; set; }
+        public int ReadChars { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Book() { }
+
+        public Book(string name, int totalChars)
+        {
+            Name = name;
+            TotalChars = totalChars;
+        }
+
+        public double CalculatePercentRead()
+        {
+            if (TotalChars == 0) return 0;
+            return (double)ReadChars / TotalChars * 100;
+        }
+
+        public void UpdateProgress(int charsRead)
+        {
+            ReadChars += charsRead;
+            if (ReadChars >= TotalChars)
+            {
+                ReadChars = TotalChars;
+                IsCompleted = true;
+            }
+        }
+    }
+}
