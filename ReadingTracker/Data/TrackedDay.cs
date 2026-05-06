4b277024a87dbfca9b117d39f3a659e5fd676219
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-06 11:18:10 -0300

    Move TrackedDay to Data

---
+++ ReadingTracker/Data/TrackedDay.cs
@@ -0,0 +1,20 @@
+﻿namespace ReadingTracker.Data
+{
+    internal class TrackedDay
+    {
+        public Book Book { get; set; } = null!; 
+        public DateOnly Date { get; set; }
+        public int CharsRead { get; set; }
+        public double MinutesRead { get; set; }
+
+        public TrackedDay() { }
+
+        public TrackedDay(Book book, DateOnly date, int charsRead, double minutesRead)
+        {
+            Book = book;
+            Date = date;
+            CharsRead = charsRead;
+            MinutesRead = minutesRead;
+        }
+    }
+}
