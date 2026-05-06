a69823eb91ce54087d265b078850dda0ddf8c97d
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-06 11:18:17 -0300

    Create Series class

---
+++ ReadingTracker/Data/Series.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ReadingTracker.Data
+{
+    internal class Series
+    {
+        public int SeriesID { get; set; } = Guid.NewGuid().GetHashCode();
+        public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public List<Genre> Genre { get; set; } = [];
+        public List<Book> Books { get; set; } = [];
+
+        public Series() { }
+    }
+}
