8db47929f59d8f465f9ee1f764901a38532cc06d
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-11 14:04:36 -0300

    Add methods for series class

--- ReadingTracker/Data/Series.cs
+++ ReadingTracker/Data/Series.cs
@@ -7,11 +7,31 @@ namespace ReadingTracker.Data
 {
     internal class Series
     {
-        public int SeriesID { get; set; } = Guid.NewGuid().GetHashCode();
-        public string Name { get; set; } = string.Empty;
-        public string Author { get; set; } = string.Empty;
-        public List<Genre> Genre { get; set; } = [];
-        public List<Book> Books { get; set; } = [];
+        public int SeriesID { get; private set; } = Guid.NewGuid().GetHashCode();
+        public string Name { get; private set; } = string.Empty;
+        public string Author { get; private set; } = string.Empty;
+        private List<Genre> Genre { get; set; } = [];
+        private List<Book> Books { get; set; } = [];
+
+        public List<Genre> GetGenres() => Genre;
+
+        public List<Book> GetBooks() => Books;
+
+        public void AddGenre(Genre genre)
+        {
+            if (!Genre.Contains(genre))
+            {
+                Genre.Add(genre);
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            if (!Books.Any(b => b.BookID == book.BookID))
+            {
+                Books.Add(book);
+            }
+        }
 
         public Series() { }
     }
