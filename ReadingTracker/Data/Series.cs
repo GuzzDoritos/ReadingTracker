c831dd9789eaa281d9b3180aa550a8d584415930
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-11 14:05:10 -0300

    Remove unnecessary imports

--- ReadingTracker/Data/Series.cs
+++ ReadingTracker/Data/Series.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Text;
-
-namespace ReadingTracker.Data
+﻿namespace ReadingTracker.Data
 {
     internal class Series
     {
