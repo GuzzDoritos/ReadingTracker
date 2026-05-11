4ac7886fb107abf0f8eda420438da49ed36887bc
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-11 14:03:05 -0300

    Remove hardcoded data

--- ReadingTracker/Program.cs
+++ ReadingTracker/Program.cs
@@ -10,12 +10,4 @@ Tracker tracker = new(bookLibrary);
 
 FileService.Load(tracker);
 
-Series s = new();
-s.Name = "Rezero";
-s.Author = "Tappei";
-s.Genre.Add(Genre.Fantasy);
-s.Books.Add(bookLibrary.GetBookList()[0]);
-
-Console.WriteLine($"{s.Name}, {s.Author}, {s.SeriesID}, {string.Join(", ", s.Genre)}, {string.Join(", ", s.Books.ConvertAll(b => b.Name))}");
-
 ConsoleUI.Start(tracker);
