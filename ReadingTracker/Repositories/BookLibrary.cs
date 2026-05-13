4f3582bf336d78dd63210d18501070645860537b
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-13 14:27:46 -0300

    Update AddBook method and add method to remove books

--- ReadingTracker/Repositories/BookLibrary.cs
+++ ReadingTracker/Repositories/BookLibrary.cs
@@ -5,11 +5,16 @@ namespace ReadingTracker.Repositories
     internal class BookLibrary
     {
         private readonly List<Book> _bookList = [];
-        public void AddBook(string name, int totalChars)
+        public void AddBook(string name, string author, Genre genre, int totalChars)
             {
-                Book book = new(name, totalChars);
+                Book book = new(name, author, genre, totalChars);
                 _bookList.Add(book);
             }
+
+        public void RemoveBook(Book book)
+        {
+            _bookList.Remove(book);
+        }
         public List<Book> GetBookList() => _bookList;
 
         public void Load(List<Book> Books)
