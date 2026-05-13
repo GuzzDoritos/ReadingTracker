af4896e035213784a5ca0ea9404765708128a458
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-13 14:27:22 -0300

    Add new Author and BookGenre attributes

--- ReadingTracker/Data/Book.cs
+++ ReadingTracker/Data/Book.cs
@@ -4,15 +4,19 @@
     {
         public int BookID { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public Genre BookGenre { get; set; }
         public int TotalChars { get; set; }
         public int ReadChars { get; set; }
         public bool IsCompleted { get; set; }
 
         public Book() { }
 
-        internal Book(string name, int totalChars)
+        internal Book(string name, string author, Genre genre, int totalChars)
         {
             Name = name;
+            Author = author;
+            BookGenre = genre;
             TotalChars = totalChars;
         }
 
